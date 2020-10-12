0386c87b3ace15ec76e290aac058465c43445001
Atieh Hamidi <hamidiatiye@gmail.com>
2020-10-12 18:56:24 +0200

    installed Onbording and added 3 images for onbording

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Onboarding from "react-native-onboarding-swiper";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello my first app!</Text>
+      <Onboarding
+        onSkip={() => navigation.replace("Login")}
+        onDone={() => navigation.navigate("Login")}
+        pages={[
+          {
+            backgroundColor: "#7eeffa",
+            image: <Image source={require("./assets/onboarding-1.png")} />,
+            title: "Onboarding",
+            subtitle: "Done with React Native Onboarding Swiper",
+          },
+          {
+            backgroundColor: "#7eeffa",
+            image: <Image source={require("./assets/onboarding-2.png")} />,
+            title: "One Click",
+            subtitle: "Done with React Native Onboarding Swiper",
+          },
+          {
+            backgroundColor: "#7eeffa",
+            image: <Image source={require("./assets/onboarding-3.png")} />,
+            title: "Ask The Expert",
+            subtitle: "Done with React Native Onboarding Swiper",
+          },
+        ]}
+      />
       <StatusBar style="auto" />
     </View>
   );
