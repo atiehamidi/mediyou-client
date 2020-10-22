4040147202781c13a0b54c8fe3b692a3c62a627b
Atieh Hamidi <hamidiatiye@gmail.com>
2020-10-22 10:59:50 +0200

    added style for first page of signin screens

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, Button } from "react-native";
+import { StyleSheet, Text, View, Image, Button, Alert } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function LoginScreen() {
@@ -14,7 +14,7 @@ export default function LoginScreen() {
     >
       <LinearGradient
         // Background Linear Gradient
-        colors={["rgba(255, 255, 255, 0.5)", "transparent"]}
+        colors={["rgba(255, 255, 255, 0.8)", "transparent"]}
         style={{
           position: "absolute",
           left: 0,
@@ -23,21 +23,48 @@ export default function LoginScreen() {
           height: 400,
         }}
       />
+      <Image
+        style={{ width: 100, height: 100, marginBottom: 50 }}
+        source={require("../assets/logo-mediyou.png")}
+      />
+      <Text>WELCOME</Text>
+      <Text style={{ marginBottom: 25, color: "#ACACAC" }}>
+        sign to continue
+      </Text>
+      <LinearGradient
+        // Button Linear Gradient
+        colors={["#00AEEF", "#0080B0"]}
+        style={{
+          width: 300,
+          padding: 10,
+          alignItems: "center",
+          borderRadius: 30,
+        }}
+      >
+        <Button
+          title="Sign in with Mobile number"
+          color="#fff"
+          onPress={() => Alert.alert("Simple Button pressed")}
+        />
+      </LinearGradient>
+      <Text style={{ margin: 8, color: "#ACACAC" }}>OR</Text>
       <LinearGradient
         // Button Linear Gradient
         colors={["#4c669f", "#3b5998", "#192f6a"]}
-        style={{ padding: 15, alignItems: "center", borderRadius: 5 }}
+        style={{
+          width: 300,
+          padding: 10,
+          alignItems: "center",
+          borderRadius: 30,
+        }}
       >
-        <Text
-          style={{
-            backgroundColor: "transparent",
-            fontSize: 15,
-            color: "#fff",
-          }}
-        >
-          Sign in with Facebook
-        </Text>
+        <Button
+          title="Sign in with facebook"
+          color="#fff"
+          onPress={() => Alert.alert("Simple Button pressed")}
+        />
       </LinearGradient>
+      <Button title="Trouble signing in?" />
     </View>
   );
 }
