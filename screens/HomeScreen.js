3072d10ceb26d3b8c3c6e1b6687a3fa0e807d11c
Atieh Hamidi <hamidiatiye@gmail.com>
2020-10-28 10:08:53 +0100

    installed expo/vector-icons

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View, Image, Button, TextInput } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { Ionicons } from "@expo/vector-icons";
+
+function HomeScreen() {
+  return (
+    <View>
+      <Ionicons name="md-checkmark-circle" size={32} color="green" />;
+    </View>
+  );
+}
+
+export default HomeScreen;
